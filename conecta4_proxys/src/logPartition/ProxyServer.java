6982b761887e6f65c7c2d6d4a1d75069676dc0fd
Ruben <rgm177@alumnos.unican.es>
2023-05-12 00:22:44 +0200

    practica con proxy servant implementada con sockets UDP, primer ejercicio resuelto

---
+++ conecta4_proxys/src/logPartition/ProxyServer.java
@@ -0,0 +1,104 @@
+package logPartition;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+//import java.io.DataInputStream;
+//import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+//import java.net.ServerSocket;
+//import java.net.Socket;
+
+import common.ILogService;
+
+public class ProxyServer extends Thread{
+
+	private ILogService lSer;
+	private int SOCKET = 20010;
+	private InetAddress SERVER_ADDR;
+	
+	private DatagramSocket server = null;
+	private DatagramPacket packet;
+	private byte[] data;
+	private byte[] replyData;
+
+	public ProxyServer(ILogService logService){
+		this.lSer = logService;
+		try{
+			SERVER_ADDR = InetAddress.getByName("127.0.0.1");
+			//this.setDaemon(true);
+			start();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void run(){
+		try{
+
+			//clientSocket = serverSocket.accept();
+
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			server = new DatagramSocket();
+
+			DataOutputStream dos = new DataOutputStream(baos);
+			
+			
+			data=new byte[500];
+			packet=new DatagramPacket(data,500);
+			server.receive(packet);
+			
+			DataInputStream dis = new DataInputStream(new ByteArrayInputStream(data));
+			
+
+			int id= dis.readInt();
+
+			switch(id){
+			
+			case 0:
+				//lee los parametros del logData
+				String winner = dis.readUTF();
+				String oponent = dis.readUTF();
+				long score = dis.readLong();
+				
+				//llama a logData
+				boolean res = lSer.logData(winner, oponent, score);
+				//escribe el booleano y lo envia
+				dos.writeBoolean(res);
+				dos.flush();
+				
+				
+				data=baos.toByteArray();
+				packet=new DatagramPacket(data, data.length,SERVER_ADDR, SOCKET);
+				server.send(packet);
+
+				break;
+
+			case 1:
+				
+				long size = lSer.getLogSize();
+				dos.writeLong(size);
+				dos.flush();
+				data=baos.toByteArray();
+				packet=new DatagramPacket(data, data.length,InetAddress.getLocalHost(), SOCKET);
+				server.send(packet);
+				break;
+
+
+			}
+			dos.close();
+			dis.close();
+			
+
+		}catch(IOException e){
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	 
+	}
+
+}
---
+++ conecta4_proxys/src/playPartition/ProxyClient.java
@@ -0,0 +1,111 @@
+package playPartition;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ProxyClient {
+
+    private static final String FOLLOWER_IP = "127.0.0.1";
+    private static final int PORT = 20010;
+    private InetAddress SERVER_ADDRESS;
+
+    
+    private DatagramSocket serv = null;
+	private DatagramPacket packet;
+	private byte[] data;
+	
+	
+    public ProxyClient(){
+        try{
+            SERVER_ADDRESS=InetAddress.getByName(FOLLOWER_IP);
+        } catch(UnknownHostException e){
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+    
+    
+    public boolean logData(String winner, String opponent, long score){
+        
+        boolean res = false;
+
+        try{
+        	//System.out.println("funcion logData");
+            //Socket server = new Socket(InetAddress.getLocalHost(),PORT);
+        	serv=new DatagramSocket();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            
+            DataOutputStream dos = new DataOutputStream(baos);
+            
+            data = new byte[winner.length()+opponent.length()+Long.BYTES];
+            packet = new DatagramPacket(data,data.length);
+            
+            dos.writeInt(0);
+            //System.out.println("funcion logData");
+            dos.writeUTF(winner);
+			dos.writeUTF(opponent);
+			dos.writeLong(score);
+
+            dos.flush();
+            data=baos.toByteArray();
+            packet=new DatagramPacket(data, data.length,SERVER_ADDRESS,PORT);
+            serv.send(packet);
+            
+            data=new byte[1];
+            DataInputStream dis = new DataInputStream(new ByteArrayInputStream(data));
+            res= dis.readBoolean();
+
+            
+        }catch(IOException e){
+            e.printStackTrace();
+            System.exit(-1);
+
+        }
+        return res;
+
+    }
+
+
+    public long getLogSize(){
+    	System.out.println("funcion logSize");
+        long tam=0;
+        try {
+			//Socket socket = new Socket(InetAddress.getLocalHost(),PORT);
+			serv = new DatagramSocket();
+        	ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			
+        	DataOutputStream dos = new DataOutputStream(baos);
+			
+			
+			dos.writeInt(1);//si es 1 significa que es la funcion showStatsInfo
+			dos.flush();
+			
+			data=baos.toByteArray();
+			
+			data = new byte[Long.BYTES];
+			packet = new DatagramPacket(data, data.length,SERVER_ADDRESS, PORT);
+			serv.send(packet);
+			
+			data = new byte[Long.BYTES];
+			DataInputStream dis = new DataInputStream(new ByteArrayInputStream(data));
+			tam = dis.readLong();//llamada bloqueante
+			System.err.println("(DEBUG) Log size: "+tam+" bytes");
+			
+			//liberamos recursos
+			dos.close();
+			dis.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+        return tam;
+    }
+}
